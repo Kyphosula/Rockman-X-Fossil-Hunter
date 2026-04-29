a8fae4b5af0156854ad9c234cfe9ffa7344c177e
Scopsula <scopsula@tuta.com>
2026-04-29 23:37:04 +0100

    demo met-like logic

--- src/npcs/met.nim
+++ src/npcs/met.nim
@@ -1,17 +1,33 @@
 import ../entities
 
-proc metEnemy*(npc, player: base): updateNpc =
-  var update: updateNpc
+var metTimer: int
 
-  if player.pos[0] + player.colX2 / 2 < npc.pos[0] + npc.colX2 / 2:
-    if npc.facing == 1: 
+proc metEnemy*(npc, target: base): updateNpc =
+  var update: updateNpc
+  var name: string = "Met"
+
+  if metTimer > 0:
+    metTimer -= 1
+  else:
+    update.updateNeeded = true
+
+  if player(target).fire:
+    if target.pos[1] + target.colY2 >= npc.pos[1] + npc.colY2:
+      if target.pos[1] + target.colY2 <= npc.pos[1] + npc.colY2 + 24:
+        name = name & "_HIDE"
+        npc.textureName = name
+        update.updateNeeded = true
+        metTimer = 20
+
+  if target.pos[0] + target.colX2 / 2 < npc.pos[0] + npc.colX2 / 2:
+    if npc.facing == 1 or update.updateNeeded: 
       npc.facing = -1
-      npc.textureName = "Met_LEFT"
+      npc.textureName = name & "_LEFT"
       update.updateNeeded = true
   else: 
-    if npc.facing == -1:
+    if npc.facing == -1 or update.updateNeeded:
       npc.facing = 1
-      npc.textureName = "Met_RIGHT"
+      npc.textureName = name & "_RIGHT"
       update.updateNeeded = true
   
   if update.updateNeeded:
